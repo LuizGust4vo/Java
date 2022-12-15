65350198eb334a63fdafa51b91ea758d2752a6eb
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-12-15 00:10:00 -0300

    Update Entrega_cartao_app.java

--- Praticando Orientação a Objetos com Java/Entrega_cartao_app.java
+++ Praticando Orientação a Objetos com Java/Entrega_cartao_app.java
@@ -1,6 +1,5 @@
 package poo;
 
-import java.util.ArrayList;
 import poo_model.Cliente;
 import poo_model.Endereço;
 
@@ -9,16 +8,21 @@ public class Entrega_cartao_app {
 	public static void main(String[] args) {
 
 		Endereço endereco = new Endereço();
+		endereco.setCep("0000000");
 		// Dados do endereço
 
 		Cliente cliente = new Cliente();
 		// Dados do cliente
-		if (cliente.enderecos == null) {
-			cliente.enderecos = new ArrayList<Endereço>();
+
+		// cliente.adicionaEndereco(endereco);
+		// System.out.println ("Endereço adicionado com sucesso!");
+
+		// Também é possível utilizar try-catch para exibir o erro
+		try {
+			cliente.adicionaEndereco(endereco);
+			System.out.println("Endereço adicionado com sucesso!");
+		} catch (Exception e) {
+			System.err.println("Houve um erro ao adicionar endereço: " + e.getMessage());
 		}
-
-		cliente.enderecos.add(endereco);
-		System.out.println("Endereço adicionado com sucesso!");
 	}
-
 }
