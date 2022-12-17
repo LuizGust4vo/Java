080eeca95c7ebcf22037a571d7ac5fe512cfc95c
Luiz Gustavo <lalmeida727.lg@gmail.com>
2022-12-17 10:01:51 -0300

    Create Teste.java

---
+++ Aulas de OO - FGA/q1/Teste.java
@@ -0,0 +1,11 @@
+package q1;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Mulher pessoa = new Mulher("08319156173","Maria", 0.10);
+
+		Cliente cliente1 = new Cliente (0.5);
+	}
+
+}
